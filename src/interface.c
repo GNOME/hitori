3623148f1b35a4086c2e7db65978dd9554e240ff
Philip Withnall <philip@tecnocode.co.uk>
2010-10-28 16:28:54 +0100

    Remove non-translatable text from a translatable message

--- src/interface.c
+++ src/interface.c
@@ -61,7 +61,7 @@ hitori_create_interface (Hitori *hitori)
 				GTK_DIALOG_MODAL,
 				GTK_MESSAGE_ERROR,
 				GTK_BUTTONS_OK,
-				_("UI file “%s/hitori/hitori.ui” could not be loaded"), PACKAGE_DATA_DIR);
+				_("UI file “%s” could not be loaded"), PACKAGE_DATA_DIR"/hitori/hitori.ui");
 		gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (dialog), "%s", error->message);
 		gtk_dialog_run (GTK_DIALOG (dialog));
 		gtk_widget_destroy (dialog);
