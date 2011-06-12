0bcb8873a2e885247d9ec959a830d0ac3d7ded52
Philip Withnall <philip@tecnocode.co.uk>
2011-06-12 19:52:40 +0100

    Bug 652399 — l10n doesn't work
    Commit 997cf9973f4fc81ce6df531e03c044016e4d7842 means that ENABLE_NLS is
    now *never* defined, which effectively removed runtime gettext support from
    Hitori.
    
    Closes: bgo#652399

--- src/main.c
+++ src/main.c
@@ -229,11 +229,9 @@ main (int argc, char *argv[])
 	        { NULL }
 	};
 
-#ifdef ENABLE_NLS
 	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
-#endif
 
 	gtk_init (&argc, &argv);
 	g_set_application_name (_("Hitori"));
