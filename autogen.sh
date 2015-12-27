70a475367f770928bfa3539f6dd4253eea090d4b
Sahil Sareen <sahil.sareen@hotmail.com>
2015-12-27 19:24:28 +0530

    Add a pre-commit and pre-applypatch git hook

--- autogen.sh
+++ autogen.sh
@@ -8,6 +8,16 @@ test -z "$srcdir" && srcdir=.
 	exit 1
 }
 
+# Use the style-checker as pre-commit and pre-applypatch hooks
+if [ -d $srcdir/.git ]; then
+        if [ ! -L $srcdir/.git/hooks/pre-commit ]; then
+            ln -s ../../../libgames-support/style-checker $srcdir/.git/hooks/pre-commit && echo "Enabled pre-commit style checker." || :
+        fi
+        if [ ! -L $srcdir/.git/hooks/pre-applypatch ]; then
+            ln -s ../../../libgames-support/style-checker $srcdir/.git/hooks/pre-applypatch && echo "Enabled pre-applypatch style checker." || :
+        fi
+fi
+
 PKG_NAME=`autoconf --trace "AC_INIT:$1" "$srcdir/configure.ac"`
 
 if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
